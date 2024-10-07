01e5069b813aa2a857e3bb9eb41d45d7aa8cb7f0
as7tix <krid4403@gmail.com>
2024-10-07 11:33:31 +0400

    Add files via upload

---
+++ GoGa v1/GoGa v1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoGa_v1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://cdnn21.img.ria.ru/images/07e4/0c/0a/1588655917_611:0:2659:2048_1280x0_80_0_0_3e0b64c5cdaccb42ccae9f4c8afe1859.jpg");
+        }
+    }
+}
